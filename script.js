914bcb84cd5d128e2ef0d47cb214b6f73c4ad2f4
cybrox834 <cybr0x83@gmail.com>
2025-10-01 15:40:07 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+const signupForm = document.getElementById('signupForm');
+
+signupForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const username = signupForm.username.value;
+  const email = signupForm.email.value;
+  const password = signupForm.password.value;
+
+  alert(`Signed up successfully!\nUsername: ${username}\nEmail: ${email}`);
+  signupForm.reset();
+});
